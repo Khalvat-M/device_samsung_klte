91958cb65b7c4209e355f1dde04c70916da782ef
Khalvat-M <mmba313@gmail.com>
2020-01-21 16:59:34 +0330

    Set build FP for klte

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2014-2016 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,6 +17,8 @@
 include device/samsung/klte-common/BoardConfigCommon.mk
 
 TARGET_OTA_ASSERT_DEVICE := klte,klteacg,klteatt,kltecan,kltelra,kltetmo,klteub,klteusc,kltevzw,kltexx
+
+BUILD_FINGERPRINT := samsung/kltexx/klte:6.0.1/MMB29M/G900FXXU1CRH1:user/release-keys
 
 # Kernel
 TARGET_KERNEL_CONFIG := lineage_klte_pn547_defconfig
